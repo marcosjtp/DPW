f8e405a2fa3c825c817b20fc3917e5fe7cb7d67c
marcosjtp <marcos.jtp@outlook.com>
2021-01-11 18:32:50 -0400

    Primera Tarea de JS

--- Semana4/practica8/codigo.js
+++ Semana4/practica8/codigo.js
@@ -15,11 +15,10 @@ function suma() {
         var num1;
         var num2;
         var resultado;
-        num1=document.getElementById("num1").value
-        num2=document.getElementById("num2").value
+        num1=document.getElementById("num1").value;
+        num2=document.getElementById("num2").value;
         resultado=parseInt(num1)-parseInt(num2);
         document.getElementById("resultado").value=resultado;
-        
     }
     function multi() {
         var num1;
