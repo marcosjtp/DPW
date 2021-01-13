c3929137e2ff8040d1ab527db22ee0da1a807f8c
marcosjtp <marcos.jtp@outlook.com>
2021-01-13 18:48:21 -0400

    cambio fondo

--- Semana4/practica8/codigo.js
+++ Semana4/practica8/codigo.js
@@ -15,9 +15,10 @@ function suma() {
         var num1;
         var num2;
         var resultado;
-        num1=document.getElementById("num1").value;
-        num2=document.getElementById("num2").value;
-        resultado=parseInt(num1)-parseInt(num2);
+        /*en este caso estamos convirtiendo el valor del input num1 en entero y asignandolo a la variable num1 */
+        num1=parseInt(document.getElementById("num1").value);
+        num2=parseInt(document.getElementById("num2").value);
+        resultado=num1-num2;
         document.getElementById("resultado").value=resultado;
     }
     function multi() {
@@ -36,7 +37,7 @@ function suma() {
         var resultado;
         num1=document.getElementById("num1").value
         num2=document.getElementById("num2").value
-        resultado=parseInt(num1)/ parseInt(num2);
+        resultado=parseFloat(num1)/ parseFloat(num2);
         document.getElementById("resultado").value=resultado;
         
     }
